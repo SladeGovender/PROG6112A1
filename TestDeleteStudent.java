157e92833bcd523e0a1c09943b5a2d1f4832bc07
Slade <128598022+SladeGovender@users.noreply.github.com>
2023-09-28 22:56:17 +0200

    Create TestDeleteStudent.java

---
+++ TestDeleteStudent.java
@@ -0,0 +1,24 @@
+public class TestDeleteStudent {
+    public static void DeleteStudent(ArrayList<Student> students, Scanner scanner){
+        
+        System.out.println("Enter the students ID to delete:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Are you sure you want to delete student " + id + "? Yes (y) to confirm ");
+        String delete = scanner.nextLine();
+        if (delete == "y")
+        {
+            for (int i = 0; i < students.size(); i++)
+            {
+                if (students.get(i).getStudentID() == id)
+                {
+                    students.remove(i);
+                    System.out.println("Student" + id + "was DELETED!!!");
+                    return;
+                }
+            }
+            System.out.println("Student ID:" + id + " was not found!!!");
+        }
+        
+    }
+}
